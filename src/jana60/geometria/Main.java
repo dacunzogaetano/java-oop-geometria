674d14d11ac42f6f7ae2fbee29ec925b9ad5928b
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-14 15:27:01 +0200

    esercizio completo

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,24 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Rettangolo uno = new Rettangolo();
+		
+		System.out.println("Inserisci la base del rettangolo: ");
+		uno.base = scan.nextInt();
+		System.out.println("Inserisci l'altezza del rettangolo: ");
+		uno.altezza = scan.nextInt();
+		
+		
+		
+		System.out.println("L'area del rettangolo è " + uno.area() + ", mentre il perimetro è " + uno.perimetro());
+		scan.close();
 	}
 
 }
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -1,5 +1,20 @@
 package jana60.geometria;
 
 public class Rettangolo {
+	
+	//attributi rettangolo
+	
+	int base;
+	int altezza;
+	
+	//metodi rettangolo
+	//area
+	int area() {
+		return base * altezza;
+	}
+	//perimetro
+	int perimetro() {
+		return (base + altezza) * 2;
+	}
 
 }
